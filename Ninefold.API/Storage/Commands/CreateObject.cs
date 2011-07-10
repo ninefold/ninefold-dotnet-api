43d3717039175a3bb09df345ae0f95b4039a429d
Steve Godbold <stephen.godbold@gmail.com>
2011-07-10 09:04:08 -0700

    Added request body, and standard headers

--- Ninefold.API/Storage/Commands/CreateObject.cs
+++ Ninefold.API/Storage/Commands/CreateObject.cs
@@ -12,6 +12,10 @@ namespace Ninefold.API.Storage.Commands
     {
         readonly INinefoldService _storageService;
 
+        public byte[] Content { get; set; }
+
+        public string ContentType { get; set; }
+
         public string UserId { get; set; }
 
         public string GroupACL { get; set; }
@@ -32,21 +36,27 @@ namespace Ninefold.API.Storage.Commands
         public CreateObjectResponse Execute()
         {
             var request = new RestRequest("rest/objects", Method.POST);
+            request.AddHeader("content-type", ContentType);
+            request.AddHeader("content-length", Content.Length.ToString());
             request.AddHeader("x-emc-date", DateTime.UtcNow.ToString());
             request.AddHeader("x-emc-groupacl", string.Format("other={0}", GroupACL));
             request.AddHeader("x-emc-useracl", BuildKeyPairString(ACL));
             request.AddHeader("x-emc-listable-meta", BuildKeyPairString(ListableMetadata));
             request.AddHeader("x-emc-meta", BuildKeyPairString(Metadata));
             request.AddHeader("x-emc-uid", UserId);
-
+            
             foreach (var optionalHeader in OptionalHeaders)
             {
                 request.AddHeader(optionalHeader.Key, optionalHeader.Value);
             }
 
+            request.AddBody(Content);
+
             return _storageService.ExecuteRequest<CreateObjectResponse>(request);
         }
+
         
+
         private static string BuildKeyPairString(IEnumerable<KeyValuePair<string, string>> keyValuePairs)
         {
             if ((keyValuePairs == null) || (keyValuePairs.Count() == 0)) return string.Empty;
