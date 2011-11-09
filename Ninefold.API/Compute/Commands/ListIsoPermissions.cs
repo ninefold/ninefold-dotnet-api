c9c909ab8f5855bc3557f327ac5e7f90ec8da77c
Steve Godbold <stephen.godbold@gmail.com>
2011-11-09 15:21:56 +1100

    Added list iso permissions command

--- Ninefold.API/Compute/Commands/ListIsoPermissions.cs
+++ Ninefold.API/Compute/Commands/ListIsoPermissions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Xml.Linq;
 using Ninefold.Compute.Messages;
 using Ninefold.Core;
@@ -31,25 +33,25 @@ namespace Ninefold.Compute.Commands
             return (HttpWebRequest) _builder.GenerateRequest(Parameters, _authenticator, _baseUri, _apiKey, _secret);
         }
 
-        public ICommandResponse ParseResponse(WebResponse webResponse)
+        public ICommandResponse ParseResponse(System.Net.WebResponse webResponse)
         {
             var responseStream = webResponse.GetResponseStream();
             if ((responseStream == null) || (!responseStream.CanRead)) return new ListIsoPermissionsResponse();
 
             var responseDocument = XDocument.Load(responseStream);
             var templatePermission = responseDocument.Elements().Elements()
-                    .Where(e => e.Name.LocalName.Equals("templatePermission", StringComparison.InvariantCultureIgnoreCase))
+                    .Where(e => e.Name.LocalName.Equals("templatepermission", StringComparison.InvariantCultureIgnoreCase))
                     .FirstOrDefault();
 
             if (templatePermission == null) return new ListIsoPermissionsResponse();
 
             return new ListIsoPermissionsResponse
-                          {
-                              Id = int.Parse(templatePermission.ExtractValue("id")),
-                              Accounts = templatePermission.ExtractValue("accounts"),
-                              DomainId = int.Parse(templatePermission.ExtractValue("domainId")),
-                              IsPublic = bool.Parse(templatePermission.ExtractValue("isPublic"))
-                          };
+            {
+                Id = int.Parse(templatePermission.ExtractValue("id")),
+                Accounts = templatePermission.ExtractValue("accounts"),
+                DomainId = int.Parse(templatePermission.ExtractValue("domainId")),
+                IsPublic = bool.Parse(templatePermission.ExtractValue("isPublic"))
+            };
         }
     }
 }
